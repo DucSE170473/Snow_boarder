0f0712b53bcfac67ef52d0e6e7b78f69b3fc18b6
MQuyen123 <minhquyenho09@gmail.com>
2025-03-27 15:45:33 +0700

    Tạo background

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private float startPosX, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPosX = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x; // Lấy chiều dài của background
+    }
+
+    void FixedUpdate()
+    {
+        float distanceX = cam.transform.position.x * parallaxEffect;
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPosX + distanceX, transform.position.y, transform.position.z);
+
+        // Kiểm tra nếu camera đã di chuyển quá xa so với vị trí ban đầu của background
+        if (temp > startPosX + length)
+        {
+            startPosX += length; // Dịch nền sang phải
+        }
+    }
+}
